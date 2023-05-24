547f98777b136f22f33438ad98decea423c15988
Chao Xin Nhin <chaoxin.nhin@gmail.com>
2023-05-25 02:18:40 +1200

    cloud commit

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import router from "./routes/index";
-import express from "express";
-
-const app = express();
-app.use(express.json());
-
-app.use("/api/car_value", router);
-
-export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import env from "dotenv";
-
+import router from "./routes/index";
 
 env.config();
 
 const server = express();
 server.use(express.json());
+
+server.use("/api/car_value", router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
