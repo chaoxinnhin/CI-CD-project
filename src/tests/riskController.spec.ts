7cf85469fef2377fe2b65da12fc072e155a2f37c
gregchang2 <gregc@missionreadyhq.com>
2023-05-25 02:49:38 +1200

    added workflow part 6

--- src/tests/riskController.spec.ts
+++ src/tests/riskController.spec.ts
@@ -1,8 +1,5 @@
 import { createRequest, createResponse } from "node-mocks-http";
-import {
-  getAllClaims,
-  getOneClaim,
-} from "../../controller/riskratingController";
+import { getAllClaims, getOneClaim } from "../controller/riskratingController";
 
 describe("getAllClaims", () => {
   test("should send all tasks via res", () => {
