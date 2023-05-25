741bfb0deb41af443e2e1d42ca04844a579de653
Chao Xin Nhin <chaoxin.nhin@gmail.com>
2023-05-26 00:46:19 +1200

    feature get commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,6 @@ import * as carValueController from "../controller/carValueController";
 
 const router = express.Router();
 
-router.get("/", carValueController.carValidationGrace);
-
-router.get(`/${carValueController.calculateCarValue}`);
+router.get("/", carValueController.calculateCarValue);
 
 export default router;
