c7b6a3909d297f1d7440add191a3aea59a94ff6f
Chao Xin Nhin <chaoxin.nhin@gmail.com>
2023-05-26 01:42:30 +1200

    post commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import * as carValueController from "../controller/carValueController";
 
 const router = express.Router();
-
+router.post("/", carValueController.calculateCarValue);
 router.get("/", carValueController.calculateCarValue);
 
 export default router;
